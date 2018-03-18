b0bb9b81050715e4b6f98fac69d6ddc59ba9157a
daniel konimbo <daniel@konimbo.co.il>
2018-03-18 14:59:07 +0200

    first navigation bootstrap progress

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles/navigation.css';
+import { ButtonToolbar, ButtonGroup, Button, Glyphicon } from 'react-bootstrap';
 
 class Navigation extends React.Component {
   render() {
     return (
       <div id="navigation">
-        Navigation: left | right | top | bottom
-        <hr />
+        <ButtonToolbar>
+          <ButtonGroup>
+            <Button>
+              <Glyphicon glyph="arrow-left" />
+            </Button>
+            <Button>
+            <Glyphicon glyph="arrow-right" />
+            </Button>
+            <Button>
+              <Glyphicon glyph="arrow-up" />
+            </Button>
+            <Button>
+              <Glyphicon glyph="arrow-down" />
+            </Button>
+          </ButtonGroup>
+        </ButtonToolbar>
       </div>
     );
   }
